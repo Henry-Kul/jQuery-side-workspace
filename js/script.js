1ba77ec584839e33e8c3ad2b1952e9a6086353a2
Henrikas Kulikov <henrikas.kulikov@gmail.com>
2020-05-12 14:03:42 +0000

    mini challenge solution

--- js/script.js
+++ js/script.js
@@ -1,9 +1,18 @@
-$(function () {
+/*$(function () {
   // $("ul ul:first").append("<li>I'm gonna be the last sub-item</li>");
   //$(".red-box").after("<div class='red-box'>Another Red</div>")
-
   /*$(".blue-box").before(function() {
       return "<div class='blue-box'>Blue 2</div>";
-  });*/
-  $("p").after($("#list"));
+  });
+  // $("p").after($("#list"));
+
+
+});*/
+
+$(function () {
+  $(".red-box").after("<div class='red-box'>Red</div>");
+
+  $(".green-box").after("<div class='green-box'>Green</div>");
+
+  $(".blue-box").after("<div class='blue-box'>Blue</div>");
 });
