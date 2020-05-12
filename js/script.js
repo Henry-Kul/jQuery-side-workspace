c5f791a10c0d005c036c965dcb8ba4db78942d45
Henrikas Kulikov <henrikas.kulikov@gmail.com>
2020-05-12 13:02:35 +0000

    filter lecture

--- js/script.js
+++ js/script.js
@@ -1,7 +1,11 @@
 $(function () {
-  // $("#list").find("li").css("background-color", "rgba(180, 180, 30, 0.8)");
-  // $("#list").children("li").css("background-color", "rgba(180, 180, 30, 0.8)");
-  // $("#list").parents("body").css("background-color", "rgba(180, 180, 30, 0.8)");
-  // $("#list").parent().css("background-color", "rgba(180, 180, 30, 0.8)");
-  $("#list").siblings(":header").css("background-color", "rgba(180, 180, 30, 0.8)");
+  // $("#list").find("li").filter(":even").css("background-color", "rgba(180, 180, 30, 0.8)");
+  // $("li")
+  //  .filter(function (index) {
+  //   return index % 3 === 1;
+  //  })
+  //  .css("background-color", "rgba(180, 180, 30, 0.8)");
+  //  $("li").first().css("background-color", "rgba(180, 180, 30, 0.8)");
+  // $("li").eq(-2).css("background-color", "rgba(180, 180, 30, 0.8)");
+  $("li").not("#list ul li").css("background-color", "rgba(180, 180, 30, 0.8)");
 });
