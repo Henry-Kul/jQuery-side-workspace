2a0f0b514c9bd8225c0ea4a1526b4b489e602acb
Henrikas Kulikov <henrikas.kulikov@gmail.com>
2020-05-12 12:46:40 +0000

    new lecture

--- js/script.js
+++ js/script.js
@@ -1,11 +1,7 @@
 $(function () {
-  // $("p").css("background-color", "rgba(180, 180, 30, 0.8");
-  // $(".red-box").css("background-color", "rgba(180, 180, 30, 0.8");
-  // $("#list").css("background-color", "rgba(180, 180, 30, 0.8");
-  // $("input[type='text']").css("background-color", "rgba(180, 180, 30, 0.8");
-  // $("h2, p, input").css("background-color", "rgba(180, 180, 30, 0.8");
-  // $("li:last").css("background-color", "rgba(180, 180, 30, 0.8");
-  // $("li:even").css("background-color", "rgba(180, 180, 30, 0.8");
-  // $("li:odd").css("background-color", "rgba(180, 180, 30, 0.8");
-  // $("input:text").css("background-color", "rgba(180, 180, 30, 0.8");
+  // $("#list").find("li").css("background-color", "rgba(180, 180, 30, 0.8)");
+  // $("#list").children("li").css("background-color", "rgba(180, 180, 30, 0.8)");
+  // $("#list").parents("body").css("background-color", "rgba(180, 180, 30, 0.8)");
+  // $("#list").parent().css("background-color", "rgba(180, 180, 30, 0.8)");
+  $("#list").siblings(":header").css("background-color", "rgba(180, 180, 30, 0.8)");
 });
