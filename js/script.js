e4b3671730f7a64c766db0ed0768286d0d5805d6
Henrikas Kulikov <henrikas.kulikov@gmail.com>
2020-05-25 18:53:51 +0000

    image loop lesson files

--- js/script.js
+++ js/script.js
@@ -1,3 +1,26 @@
 $(function () {
-  
+  //.on("click", function() {. . .})
+  function logEvent() {
+    console.log("Mouse was clicked or key was pressed");
+  }
+
+  $("html").on("click keydown", logEvent);
+
+  var images = [
+    "images/laptop-mobile_small.jpg",
+    "images/laptop-on-table_small.jpg",
+    "images/people-office-group-team_small.jpg",
+  ];
+
+  var galleryImage = $(".gallery").find("img");
+  galleryImage.on("click", imageLoop);
+
+  var i = 0;
+
+  function imageLoop() {
+        i = (i + 1) % images.length;
+      $(galleryImage).fadeOut(function () {
+        $(galleryImage).attr("src", images[i]).fadeIn();
+    });
+  }
 });
