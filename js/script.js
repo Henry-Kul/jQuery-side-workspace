5e886fcb1d370c290f30613e461bc052faf3ddcf
Henrikas Kulikov <henrikas.kulikov@gmail.com>
2020-05-27 17:00:33 +0000

    change event

--- js/script.js
+++ js/script.js
@@ -1,19 +1,15 @@
 $(function () {
-  var inputFields = $("input:text, input:password, textarea");
-  inputFields.focus(function () {
-    $(this).css("box-shadow", "0 0 4px #666");
-  });
-
-  inputFields.blur(function () {
-    $(this).css("box-shadow", "none");
-  });
-
-  $("#name").blur(function () {
-    var text = $(this).val();
-    if (text.length < 3) {
-      $(this).css("box-shadow", "0 0 4px #811");
+  $("#checkbox").change(function () {
+    var isChecked = $(this).is(":checked"); // prop("checked")
+    if (isChecked) {
+      $(this).add("label[for='cb']").css("box-shadow", "0 0 4px #181");
     } else {
-      $(this).css("box-shadow", "0 0 4px #181");
+      $(this).add("label[for='cb']").css("box-shadow", "0 0 4px #811");
     }
+  });
+
+  $("#selection").change(function () {
+    var selectedOption = $(this).find(":selected").text();
+    alert(selectedOption);
   });
 });
