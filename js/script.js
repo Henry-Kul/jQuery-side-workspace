cc6a4ec3ea9fa8b211fadd243d6fdc8475e3fc22
Henrikas Kulikov <henrikas.kulikov@gmail.com>
2020-05-27 11:51:38 +0000

    lightbox events

--- js/script.js
+++ js/script.js
@@ -1,19 +1,21 @@
 $(function () {
-  /*  $("p").click(function() {
-      $(this).slideUp();
-  });
-  $("#content").append("<p>This is a dynamically added paragraph.</p>");
-*/
-  //delegated events
+  var galleryItems = $(".gallery").find("img");
+  galleryItems.css("width", "33%").css("opacity", "0.7");
 
-  $("#content").on("click", "p", function () {
-    $(this).slideUp();
+  galleryItems.mouseenter(function () {
+    $(this).stop().fadeTo(500, 1);
   });
-  $("#content").append("<p>This is a dynamically added paragraph.</p>");
-
-  // mini-challenge mouseenter on body that delegates on li to change font color
-
-  $("body").on("mouseenter", "li", function () {
-    $(this).css("color", "blue");
+  galleryItems.mouseleave(function () {
+    $(this).stop().fadeTo(500, 0.7);
   });
+
+  galleryItems.click(function () {
+    var source = $(this).attr("src");
+    var image = $("<img>").attr("src", source).css("width", "100%");
+    $(".lightbox").empty().append(image).fadeIn(2000);
+  });
+
+  $(".lightbox").click(function() {
+      $(this).stop().fadeOut();
+  })
 });
