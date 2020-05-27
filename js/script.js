b03f2d29cdfaf0d5394e293bc5aad5f2346aa342
Henrikas Kulikov <henrikas.kulikov@gmail.com>
2020-05-27 16:38:17 +0000

    blur and focus events

--- js/script.js
+++ js/script.js
@@ -1,18 +1,19 @@
 $(function () {
-  $("html").keydown(function (event) {
-    console.log(event.which);
+  var inputFields = $("input:text, input:password, textarea");
+  inputFields.focus(function () {
+    $(this).css("box-shadow", "0 0 4px #666");
   });
 
-  var ARROW_RIGHT = 39;
-  $("html").keydown(function (event) {
-    if (event.which === ARROW_RIGHT) {
-      $(".blue-box").stop().animate(
-        {
-          marginLeft: "+=10px",
-        },
-        50
-      );
+  inputFields.blur(function () {
+    $(this).css("box-shadow", "none");
+  });
+
+  $("#name").blur(function () {
+    var text = $(this).val();
+    if (text.length < 3) {
+      $(this).css("box-shadow", "0 0 4px #811");
+    } else {
+      $(this).css("box-shadow", "0 0 4px #181");
     }
   });
 });
-// key39 is right arrow key on keyboard
